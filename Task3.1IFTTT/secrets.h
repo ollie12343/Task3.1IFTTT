37fd809a4766e58623f1615850dfc78fa1f97ebe
ollie12343 <87453519+ollie12343@users.noreply.github.com>
2024-03-25 15:59:15 +1100

    Initial Code

---
+++ Task3.1IFTTT/secrets.h
@@ -0,0 +1,5 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "Oliver's iPhone"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "12345678"	// replace MyPassword with your WiFi password
